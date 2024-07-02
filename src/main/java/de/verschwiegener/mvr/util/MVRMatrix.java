f52c6291b5f7f14bdde2410577a2926234ac6a9c
Verschwiegener <jlmadrak@web.de>
2024-07-02 05:40:22 +0200

    MVRMatrix Scale/Transform/Rotate Methods

--- src/main/java/de/verschwiegener/mvr/util/MVRMatrix.java
+++ src/main/java/de/verschwiegener/mvr/util/MVRMatrix.java
@@ -1,6 +1,12 @@
 package de.verschwiegener.mvr.util;
 
 import java.lang.Math;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+import org.joml.Options;
+import org.joml.Runtime;
 
 public class MVRMatrix {
 
@@ -81,6 +87,19 @@ public class MVRMatrix {
 		scale[2] = (float) Math.sqrt(m20 * m20 + m21 * m21 + m22 * m22);
 		return scale;
 	}
+	
+	/**
+	 * Sets Matrix Scale
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public void setScale(float x, float y, float z) {
+		m00 = x;
+        m11 = y;
+        m22 = z;
+	}
 
 	public float[] getTranslation() {
 		float[] translation = new float[3];
@@ -88,6 +107,58 @@ public class MVRMatrix {
 		translation[1] = m31;
 		translation[2] = m32;
 		return translation;
+	}
+	
+	/**
+	 * 
+	 * @param scaleFactor Scales the Position to whatever Unit is wanted
+	 * scaleFactor 1 = Millimeter
+	 * @return
+	 */
+	public float[] getTranslation(float scaleFactor) {
+		float[] translation = new float[3];
+		translation[0] = m30 / 100;
+		translation[1] = m31 / 100;
+		translation[2] = m32 / 100;
+		return translation;
+	}
+	
+	public void translate(float[] offset) {
+        m30 = m30 + offset[0];
+        m31 = m31 + offset[1];
+        m32 = m32 + offset[2];
+	}
+	
+	public void setRotationXYZ(float angleX, float angleY, float angleZ) {		
+		float sinX = (float) Math.sin(angleX);
+        float cosX = (float) Math.sin(angleX + (Math.PI * 0.5));
+        float sinY = (float) Math.sin(angleY);
+        float cosY = (float) Math.sin(angleY + (Math.PI * 0.5));
+        float sinZ = (float) Math.sin(angleZ);
+        float cosZ = (float) Math.sin(angleZ + (Math.PI * 0.5));
+        float m_sinX = -sinX;
+        float m_sinY = -sinY;
+        float m_sinZ = -sinZ;
+
+        // rotateX
+        float nm11 = cosX;
+        float nm12 = sinX;
+        float nm21 = m_sinX;
+        float nm22 = cosX;
+        // rotateY
+        float nm00 = cosY;
+        float nm01 = nm21 * m_sinY;
+        float nm02 = nm22 * m_sinY;
+        m20 = sinY;
+        m21 = nm21 * cosY;
+        m22 = nm22 * cosY;
+        // rotateZ
+        m00 = nm00 * cosZ;
+        m01 = nm01 * cosZ + nm11 * sinZ;
+        m02 = nm02 * cosZ + nm12 * sinZ;
+        m10 = nm00 * m_sinZ;
+        m11 = nm01 * m_sinZ + nm11 * cosZ;
+        m12 = nm02 * m_sinZ + nm12 * cosZ;
 	}
 
 	public float[] getRotation() {
@@ -147,6 +218,22 @@ public class MVRMatrix {
 		rotation[1] = (nm20 - nm02) / s;
 		rotation[2] = (nm01 - nm10) / s;
 		return rotation;
+	}
+	
+	/**
+	 * Returns the Matrix Rotation as Radian
+	 * @return
+	 */
+	public float[] getRotationRadians() {
+		//https://stackoverflow.com/a/15029416
+		float x = (float) Math.atan2(m21,m22);
+		float y = (float) Math.atan2(-m20, Math.sqrt(Math.pow(m21, 2) + Math.pow(m22, 2)));
+		float z = (float) Math.atan2(m10, m00);
+		return new float[] {x, y, z};
+	}
+	public float[] getRotationAngles() {
+		float[] radians = getRotationRadians();
+		return new float[] {(float) Math.toDegrees(radians[0]), (float) Math.toDegrees(radians[1]), (float) Math.toDegrees(radians[2])};
 	}
 
 	public float m00() {
@@ -247,5 +334,45 @@ public class MVRMatrix {
 		}
 		return null;
 	}
+	
+	/**
+    * Return a string representation of this matrix.
+    * <p>
+    * This method creates a new {@link DecimalFormat} on every invocation with the format string "<code>0.000E0;-</code>".
+    * 
+    * @return the string representation
+    */
+   public String toString() {
+       String str = toString(Options.NUMBER_FORMAT);
+       StringBuffer res = new StringBuffer();
+       int eIndex = Integer.MIN_VALUE;
+       for (int i = 0; i < str.length(); i++) {
+           char c = str.charAt(i);
+           if (c == 'E') {
+               eIndex = i;
+           } else if (c == ' ' && eIndex == i - 1) {
+               // workaround Java 1.4 DecimalFormat bug
+               res.append('+');
+               continue;
+           } else if (Character.isDigit(c) && eIndex == i - 1) {
+               res.append('+');
+           }
+           res.append(c);
+       }
+       return res.toString();
+   }
+   
+   /**
+    * Return a string representation of this matrix by formatting the matrix elements with the given {@link NumberFormat}.
+    * 
+    * @param formatter
+    *          the {@link NumberFormat} used to format the matrix values with
+    * @return the string representation
+    */
+   public String toString(NumberFormat formatter) {
+       return Runtime.format(m00, formatter) + " " + Runtime.format(m10, formatter) + " " + Runtime.format(m20, formatter) + " " + Runtime.format(m30, formatter) + "\n"
+            + Runtime.format(m01, formatter) + " " + Runtime.format(m11, formatter) + " " + Runtime.format(m21, formatter) + " " + Runtime.format(m31, formatter) + "\n"
+            + Runtime.format(m02, formatter) + " " + Runtime.format(m12, formatter) + " " + Runtime.format(m22, formatter) + " " + Runtime.format(m32, formatter) + "\n";
+   }
 
 }

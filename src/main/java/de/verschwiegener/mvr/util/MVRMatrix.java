49381a414ccf49c44c59fde9058f94d3d9a31808
Verschwiegener <jlmadrak@web.de>
2024-09-10 22:34:28 +0200

    Fixed Rebase

--- src/main/java/de/verschwiegener/mvr/util/MVRMatrix.java
+++ src/main/java/de/verschwiegener/mvr/util/MVRMatrix.java
@@ -92,7 +92,7 @@ public class MVRMatrix {
 		scale[2] = Math.sqrt(m20 * m20 + m21 * m21 + m22 * m22);
 		return scale;
 	}
-
+	
 	/**
 	 * Sets Matrix Scale
 	 * 
@@ -102,8 +102,8 @@ public class MVRMatrix {
 	 */
 	public void setScale(double x, double y, double z) {
 		m00 = x;
-		m11 = y;
-		m22 = z;
+        m11 = y;
+        m22 = z;
 	}
 	
 	/**
@@ -118,36 +118,21 @@ public class MVRMatrix {
 		translation[2] = m32;
 		return translation;
 	}
-
+	
 	/**
-<<<<<<< HEAD
-	 * Returns the Translation of the Matrix multiplied by scaleFactor
-=======
 	 * Returns the Translation of the Matrix divided by scaleFactor
->>>>>>> origin/main
 	 * 
-	 * @param scaleFactor Scales the Position to whatever Unit is wanted scaleFactor
-	 *                    1 = Millimeter
+	 * @param scaleFactor Scales the Position to whatever Unit is wanted
+	 * scaleFactor 1 = Millimeter
 	 * @return
 	 */
 	public double[] getTranslation(double scaleFactor) {
 		double[] translation = new double[3];
-		translation[0] = m30 * scaleFactor;
-		translation[1] = m31 * scaleFactor;
-		translation[2] = m32 * scaleFactor;
+		translation[0] = m30 / 100;
+		translation[1] = m31 / 100;
+		translation[2] = m32 / 100;
 		return translation;
 	}
-<<<<<<< HEAD
-	/**
-	 * Offsets Position by given translation
-	 * @param offset
-	 */
-	public void translate(double[] offset) {
-		m30 = m30 + offset[0];
-		m31 = m31 + offset[1];
-		m32 = m32 + offset[2];
-	}
-=======
 	
 	/**
 	 * Offsets the Position by the given Offset
@@ -176,38 +161,26 @@ public class MVRMatrix {
         double m_sinX = -sinX;
         double m_sinY = -sinY;
         double m_sinZ = -sinZ;
->>>>>>> origin/main
 
-	public void setRotationXYZ(double angleX, double angleY, double angleZ) {
-		double sinX = Math.sin(angleX);
-		double cosX = Math.sin(angleX + (Math.PI * 0.5));
-		double sinY = Math.sin(angleY);
-		double cosY = Math.sin(angleY + (Math.PI * 0.5));
-		double sinZ = Math.sin(angleZ);
-		double cosZ = Math.sin(angleZ + (Math.PI * 0.5));
-		double m_sinX = -sinX;
-		double m_sinY = -sinY;
-		double m_sinZ = -sinZ;
-
-		// rotateX
-		double nm11 = cosX;
-		double nm12 = sinX;
-		double nm21 = m_sinX;
-		double nm22 = cosX;
-		// rotateY
-		double nm00 = cosY;
-		double nm01 = nm21 * m_sinY;
-		double nm02 = nm22 * m_sinY;
-		m20 = sinY;
-		m21 = nm21 * cosY;
-		m22 = nm22 * cosY;
-		// rotateZ
-		m00 = nm00 * cosZ;
-		m01 = nm01 * cosZ + nm11 * sinZ;
-		m02 = nm02 * cosZ + nm12 * sinZ;
-		m10 = nm00 * m_sinZ;
-		m11 = nm01 * m_sinZ + nm11 * cosZ;
-		m12 = nm02 * m_sinZ + nm12 * cosZ;
+        // rotateX
+        double nm11 = cosX;
+        double nm12 = sinX;
+        double nm21 = m_sinX;
+        double nm22 = cosX;
+        // rotateY
+        double nm00 = cosY;
+        double nm01 = nm21 * m_sinY;
+        double nm02 = nm22 * m_sinY;
+        m20 = sinY;
+        m21 = nm21 * cosY;
+        m22 = nm22 * cosY;
+        // rotateZ
+        m00 = nm00 * cosZ;
+        m01 = nm01 * cosZ + nm11 * sinZ;
+        m02 = nm02 * cosZ + nm12 * sinZ;
+        m10 = nm00 * m_sinZ;
+        m11 = nm01 * m_sinZ + nm11 * cosZ;
+        m12 = nm02 * m_sinZ + nm12 * cosZ;
 	}
 
 	public double[] getRotation() {
@@ -268,31 +241,27 @@ public class MVRMatrix {
 		rotation[2] = (nm01 - nm10) / s;
 		return rotation;
 	}
-
+	
 	/**
 	 * Returns the Matrix Rotation as Radian
 	 * 
 	 * @return
 	 */
 	public double[] getRotationRadians() {
-		// https://stackoverflow.com/a/15029416
-		double x = Math.atan2(m21, m22);
+		//https://stackoverflow.com/a/15029416
+		double x = Math.atan2(m21,m22);
 		double y = Math.atan2(-m20, Math.sqrt(Math.pow(m21, 2) + Math.pow(m22, 2)));
 		double z = Math.atan2(m10, m00);
-		return new double[] { x, y, z };
+		return new double[] {x, y, z};
 	}
-<<<<<<< HEAD
-
-=======
 	/**
 	 * Returns the Rotation as Angle
 	 * 
 	 * @return
 	 */
->>>>>>> origin/main
 	public double[] getRotationAngles() {
 		double[] radians = getRotationRadians();
-		return new double[] { Math.toDegrees(radians[0]), Math.toDegrees(radians[1]), Math.toDegrees(radians[2]) };
+		return new double[] {Math.toDegrees(radians[0]), Math.toDegrees(radians[1]), Math.toDegrees(radians[2])};
 	}
 
 	public double m00() {
@@ -393,7 +362,7 @@ public class MVRMatrix {
 		}
 		return null;
 	}
-
+	
 	public String toMVR() {
 		DecimalFormat decimalFormat = new DecimalFormat("0.000000");
 		return "{" + decimalFormat.format(m00) + "," + decimalFormat.format(m10) + "," + decimalFormat.format(m20) + ","
@@ -401,53 +370,8 @@ public class MVRMatrix {
 				+ decimalFormat.format(m21) + "," + decimalFormat.format(m31) + "}{" + decimalFormat.format(m02) + ","
 				+ decimalFormat.format(m12) + "," + decimalFormat.format(m22) + "," + decimalFormat.format(m32) + "}";
 	}
-
+	
 	/**
-<<<<<<< HEAD
-	 * Return a string representation of this matrix.
-	 * <p>
-	 * This method creates a new {@link DecimalFormat} on every invocation with the
-	 * format string "<code>0.000E0;-</code>".
-	 * 
-	 * @return the string representation
-	 */
-	public String toString() {
-		String str = toString(Options.NUMBER_FORMAT);
-		StringBuffer res = new StringBuffer();
-		int eIndex = Integer.MIN_VALUE;
-		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-			if (c == 'E') {
-				eIndex = i;
-			} else if (c == ' ' && eIndex == i - 1) {
-				// workaround Java 1.4 DecimalFormat bug
-				res.append('+');
-				continue;
-			} else if (Character.isDigit(c) && eIndex == i - 1) {
-				res.append('+');
-			}
-			res.append(c);
-		}
-		return res.toString();
-	}
-
-	/**
-	 * Return a string representation of this matrix by formatting the matrix
-	 * elements with the given {@link NumberFormat}.
-	 * 
-	 * @param formatter the {@link NumberFormat} used to format the matrix values
-	 *                  with
-	 * @return the string representation
-	 */
-	public String toString(NumberFormat formatter) {
-		return Runtime.format(m00, formatter) + " " + Runtime.format(m10, formatter) + " "
-				+ Runtime.format(m20, formatter) + " " + Runtime.format(m30, formatter) + "\n"
-				+ Runtime.format(m01, formatter) + " " + Runtime.format(m11, formatter) + " "
-				+ Runtime.format(m21, formatter) + " " + Runtime.format(m31, formatter) + "\n"
-				+ Runtime.format(m02, formatter) + " " + Runtime.format(m12, formatter) + " "
-				+ Runtime.format(m22, formatter) + " " + Runtime.format(m32, formatter) + "\n";
-	}
-=======
     * Return a string representation of this matrix.
     * <p>
     * This method creates a new {@link DecimalFormat} on every invocation with the format string "<code>0.000E0;-</code>".
@@ -512,6 +436,5 @@ public class MVRMatrix {
        df = new DecimalFormat(" 0." + new String(prec) + "E0;-");
        return df;
    }
->>>>>>> origin/main
 
 }
